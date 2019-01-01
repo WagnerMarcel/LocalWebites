7c53f5376fa8a74d8b6845c85407a04db22cca8e
Marcel Wagner <marcel_wagner_@arcor.de>
2019-01-01 22:27:25 +0100

    c

--- everyDayCalendar/edc.js
+++ everyDayCalendar/edc.js
@@ -5,7 +5,7 @@ client.open('GET', '/everyDayCalendar/edc_data.json');
 client.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     myObj = JSON.parse(this.responseText);
-    txt += "<table class="table">"
+    txt += "<table class='table'>"
     for (x in myObj.days) {
       txt += "<tr><td>" + x+1 + "</td>";
       for (y in myObj.days[x]){
