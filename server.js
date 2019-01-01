d4d851e0df27ec3e1f6b4b376ae7f1397eb1dcd5
Marcel Wagner <marcel_wagner_@arcor.de>
2019-01-01 14:49:28 +0100

    c

--- server.js
+++ server.js
@@ -6,15 +6,11 @@ var url = require('url');
 
 app = express();
 app.use(express['static'](__dirname));
-app.use(express.static('everyDayCalendar'))
+app.use(express.static('public'))
 
 // Express route for incoming requests for a customer name
 app.get('/api/edc', function(req, res) {
   res.status(200).send("Working Bitch");
-});
-
-app.get('/edc', function(req, res){
-  res.status(200).sendfile('/everyDayCalendar/everyDayCalendar.index');
 });
 
 app.listen(8080);
