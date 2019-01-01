2dcff14cca801197cfd15594e45d7ee160cf6a9a
Marcel Wagner <marcel_wagner_@arcor.de>
2019-01-01 15:38:09 +0100

    c

--- server.js
+++ server.js
@@ -9,17 +9,19 @@ app.use(express['static'](__dirname));
 app.use('/edc', express.static(__dirname + '/everyDayCalendar'))
 
 // Express route for every day Calendar
-app.route('/api/edc/:day')
+app.route('/api/edc')
   .get(function(req, res) {
-    res.status(200).send('Hello' + req.params.day);
+    res.status(200).sendFile('everyDayCalendar/edc_data.json');
   })
   .post(function(req, res) {
-    storeJson('everyDayCalendar/edc_data.json', day);
+    storeJson('everyDayCalendar/edc_data.json', req.query.month, req.query.day);
     res.status(200).send('ok');
   })
 
-var storeJson = function(path, day){
-
+var storeJson = function(path, month, day){
+  var data = JSON.parse(fs.readFileSync(path).toString());
+  data.days.[month-1].[day-1] = 1;
+  fs.writeFile(path, JSON.stringify(data));
 }
 
 app.listen(8080);
